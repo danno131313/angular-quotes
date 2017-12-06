8559b6008cb7bbc4c46af09e789782bc9fa2dd45
Daniel Kavanagh <dan.kava13@gmail.com>
2017-12-05 20:41:16 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    quotes: object[];
+    quote: object;
+    constructor() {
+        this.quotes = [];
+        this.quote = {votes: 0};
+    }
+
+    compare(a, b) {
+        if (a.votes > b.votes) {
+            return -1;
+        } else if (b.votes > a.votes) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    createQuote() {
+        this.quotes.push(this.quote);
+        this.quotes.sort(this.compare);
+        this.quote = {votes: 0};
+        console.log(this.quotes);
+    }
+
+    upvote(quote) {
+        quote.votes += 1;
+        this.quotes.sort(this.compare);
+    }
+
+    downvote(quote) {
+        quote.votes -= 1;
+        this.quotes.sort(this.compare);
+    }
+
+    deleteQuote(idx: number) {
+        this.quotes.splice(idx, 1);
+    }
 }
